5d6a99ca2aaa77eff5248dc446e2eaf7c2b80e38
Armin <armin.monirzadeh@luxuryescapes.com>
2023-03-10 23:48:09 +1100

    add cards to nav drawer

--- src/app/AppNavDrawerContent.tsx
+++ src/app/AppNavDrawerContent.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
-import { List, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
+import {
+  alpha,
+  Button,
+  Card,
+  CardActions,
+  CardHeader,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Toolbar,
+  useTheme,
+} from '@mui/material'
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded'
 import WorkRoundedIcon from '@mui/icons-material/WorkRounded'
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded'
@@ -9,43 +23,69 @@ import AppLogo from './AppLogo'
 import Link from 'next/link'
 
 export default function AppNavDrawerContent() {
+  const theme = useTheme()
+
   return (
     <>
       <Toolbar>
         <AppLogo />
       </Toolbar>
-      <List>
-        <ListItemButton component={Link} href="/">
-          <ListItemIcon>
-            <DashboardRoundedIcon />
-          </ListItemIcon>
-          <ListItemText>Reporting</ListItemText>
-        </ListItemButton>
-        <ListItemButton component={Link} href="/business-trips">
-          <ListItemIcon>
-            <WorkRoundedIcon />
-          </ListItemIcon>
-          <ListItemText>Business Trips</ListItemText>
-        </ListItemButton>
-        <ListItemButton component={Link} href="/people">
-          <ListItemIcon>
-            <PeopleRoundedIcon />
-          </ListItemIcon>
-          <ListItemText>People</ListItemText>
-        </ListItemButton>
-        <ListItemButton component={Link} href="/policies">
-          <ListItemIcon>
-            <ReceiptRoundedIcon />
-          </ListItemIcon>
-          <ListItemText>Policy</ListItemText>
-        </ListItemButton>
-        <ListItemButton component={Link} href="/settings">
-          <ListItemIcon>
-            <SettingsRoundedIcon />
-          </ListItemIcon>
-          <ListItemText>Settings</ListItemText>
-        </ListItemButton>
-      </List>
+      <Stack direction="column" gap={2} px={2} pb={4} justifyContent="space-between" height="100%">
+        <Stack direction="column" gap={4}>
+          <Card elevation={0}>
+            <ListItem>
+              <ListItemText primary="Luxury Escapes" secondary="Designer" />
+            </ListItem>
+          </Card>
+
+          <List>
+            <ListItemButton component={Link} href="/">
+              <ListItemIcon>
+                <DashboardRoundedIcon />
+              </ListItemIcon>
+              <ListItemText>Reporting</ListItemText>
+            </ListItemButton>
+            <ListItemButton component={Link} href="/business-trips">
+              <ListItemIcon>
+                <WorkRoundedIcon />
+              </ListItemIcon>
+              <ListItemText>Business Trips</ListItemText>
+            </ListItemButton>
+            <ListItemButton component={Link} href="/people">
+              <ListItemIcon>
+                <PeopleRoundedIcon />
+              </ListItemIcon>
+              <ListItemText>People</ListItemText>
+            </ListItemButton>
+            <ListItemButton component={Link} href="/policies">
+              <ListItemIcon>
+                <ReceiptRoundedIcon />
+              </ListItemIcon>
+              <ListItemText>Policy</ListItemText>
+            </ListItemButton>
+            <ListItemButton component={Link} href="/settings">
+              <ListItemIcon>
+                <SettingsRoundedIcon />
+              </ListItemIcon>
+              <ListItemText>Settings</ListItemText>
+            </ListItemButton>
+          </List>
+        </Stack>
+
+        <Card elevation={0} sx={{ backgroundColor: alpha(theme.palette.primary.main, 0.08) }}>
+          <CardHeader
+            title="Book business travel"
+            titleTypographyProps={{ variant: 'h6', color: 'primary' }}
+            subheader="Find flights, hotels and transfers at the best rates."
+            subheaderTypographyProps={{ variant: 'body2' }}
+          />
+          <CardActions>
+            <Button variant="contained" color="neutral" disableElevation fullWidth>
+              Get started
+            </Button>
+          </CardActions>
+        </Card>
+      </Stack>
     </>
   )
 }
