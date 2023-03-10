0325d7318aa9217699870ccc2ebf8101c32dd7db
Armin <armin.monirzadeh@luxuryescapes.com>
2023-03-10 22:59:35 +1100

    add page links to nav drawer

--- src/components/app/AppNavDrawerContent.tsx
+++ src/components/app/AppNavDrawerContent.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { List, ListItem, ListItemIcon, ListItemText, SxProps, Toolbar } from '@mui/material'
+import { List, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
 import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded'
 import WorkRoundedIcon from '@mui/icons-material/WorkRounded'
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded'
 import ReceiptRoundedIcon from '@mui/icons-material/ReceiptRounded'
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded'
 import AppLogo from './AppLogo'
+import Link from 'next/link'
 
 export default function AppNavDrawerContent() {
   return (
@@ -14,36 +15,36 @@ export default function AppNavDrawerContent() {
         <AppLogo />
       </Toolbar>
       <List>
-        <ListItem>
+        <ListItemButton component={Link} href="/">
           <ListItemIcon>
             <DashboardRoundedIcon />
           </ListItemIcon>
           <ListItemText>Reporting</ListItemText>
-        </ListItem>
-        <ListItem>
+        </ListItemButton>
+        <ListItemButton component={Link} href="/business-trips">
           <ListItemIcon>
             <WorkRoundedIcon />
           </ListItemIcon>
           <ListItemText>Business Trips</ListItemText>
-        </ListItem>
-        <ListItem>
+        </ListItemButton>
+        <ListItemButton component={Link} href="/people">
           <ListItemIcon>
             <PeopleRoundedIcon />
           </ListItemIcon>
           <ListItemText>People</ListItemText>
-        </ListItem>
-        <ListItem>
+        </ListItemButton>
+        <ListItemButton component={Link} href="/policies">
           <ListItemIcon>
             <ReceiptRoundedIcon />
           </ListItemIcon>
           <ListItemText>Policy</ListItemText>
-        </ListItem>
-        <ListItem>
+        </ListItemButton>
+        <ListItemButton component={Link} href="/settings">
           <ListItemIcon>
             <SettingsRoundedIcon />
           </ListItemIcon>
           <ListItemText>Settings</ListItemText>
-        </ListItem>
+        </ListItemButton>
       </List>
     </>
   )
