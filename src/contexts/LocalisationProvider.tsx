14fc09d8a4abde5f108478c303b77087758a49a9
Armin <armin.monirzadeh@luxuryescapes.com>
2023-03-15 20:58:26 +1100

    add en-AU locale to date localisation

--- src/contexts/LocalisationProvider.tsx
+++ src/contexts/LocalisationProvider.tsx
@@ -1,8 +1,14 @@
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { enAU } from 'date-fns/locale'
 import React, { PropsWithChildren } from 'react'
 
 export default function LocalisationProvider(props: PropsWithChildren) {
   const { children } = props
-  return <LocalizationProvider dateAdapter={AdapterDateFns}>{children}</LocalizationProvider>
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enAU}>
+      {children}
+    </LocalizationProvider>
+  )
 }
